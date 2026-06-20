9f6cdd0fb95ecab4d38bc83125eeed33a77abe0e
chartsel <81725567+chartsel@users.noreply.github.com>
2026-06-20 17:05:58 +0000

    Initial setup and unit tests

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,14 @@
+﻿namespace FactorialLibrary.Tests;
+
+using FactorialLibrary;
+
+public class FactorialServiceTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = FactorialLibrary.FactorialService.Factorial(0);
+
+        Assert.Equal(1,result);
+    }
+}
